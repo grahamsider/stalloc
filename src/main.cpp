05212a95a412cd131e4df98df84b701f1c705a3d
grahamsider <grahamsider@gmail.com>
2023-03-19 14:14:49 -0400

    [WIP]: Clean up initial implementation

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "stalloc.hpp"
+
+int main() {
+    // stalloc_t of type int
+    stalloc_t<4096, int> istk;
+
+    // Allocate, set, and free valid pointer
+    int* mem1 = istk.alloc(sizeof(int));
+    *mem1 = 42;
+    std::cout << pr_inf << "*mem1 = " << *mem1 << std::endl;
+    istk.free(mem1);
+
+    // Try to free invalid pointer
+    istk.free(mem1 - sizeof(int));
+
+    // Try to allocate more than max size
+    mem1 = istk.alloc(4097);
+
+    // stalloc_t of unspecified type (default: void)
+    stalloc_t<4096> vstk;
+
+    // Allocate, set, and free valid array
+    const size_t mem2_size = 16;
+    int* mem2 = static_cast<int*>(vstk.alloc(mem2_size));
+    for (size_t i = 0; i < mem2_size; i++) mem2[i] = i;
+    for (size_t i = 0; i < mem2_size; i++) std::cout << pr_inf << "mem2[i] = " << mem2[i] << std::endl;
+    vstk.free(mem2);
+
+    return 0;
+}
